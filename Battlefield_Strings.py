41fc26b80ac0a05e8586bacb4e157d3e223ed926
vahanbznuni <vahan@bznuni.com>
2021-08-25 22:59:11 -0700

    Fix misspell in intro

--- Battlefield_Strings.py
+++ Battlefield_Strings.py
@@ -67,7 +67,7 @@ Then, each player will take turns targetting the opposing player's battlefield.
 if the player succesfully hits a ship, that player will have the next turn. 
 If not, the other player will have the next turn.
 
-The game is won simply by sunking the entire fleet of the opposint player."""
+The game is won simply by sinking the entire fleet of the opposint player."""
 place_ships_str = "\n\nIt is time to place your ships! Are you ready?"
 
 def intro_str():
