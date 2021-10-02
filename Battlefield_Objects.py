2704f9ff0818684b5d0b1107cd039306a62acc79
vahanbznuni <vahan@bznuni.com>
2021-10-01 22:48:14 -0700

    Add DocStrings for Ship class.

--- Battlefield_Objects.py
+++ Battlefield_Objects.py
@@ -27,6 +27,8 @@ class Battlefield:
     method (since it does not require user input), as well as the display funcitonality - so as not to display the computer's ships
     to the player (player1) when otherwise displaying the computer's battlefiled to the main player.
 
+    Class variables:
+      states (list): List of possible statuses ("states") for coordinates in grid
     Instance Variables:
       rows (list): A list of alphabetical rows the Battlefield instance is comprised of 
       columns (list): A list of columns the Battlefield instance is comprised of.
@@ -469,10 +471,31 @@ class ComputerBattlefield(Battlefield):
         return fleet
 
 class Ship:
+    """contains data structure(s) relating to each ship, including its coordinates, it's name/type, and it's health status.
+
+    Class variables:
+      types (dict): contains names of ship types as keys, and sizes for each ship as values.
+    Instance Variables:
+      coordinates (list): the coordinates of the ship.
+      type (str): the name of the ship type, from the list of ship types.
+      battlefield (object): a Battlefiled class bject - the Battlefiled to which the ship has been added.
+      size (int): the number of coordinates that a ship occupipes on the Battlefiled grid.
+      sunk (bool): whether all of the ship's coordinates have been hit.
+    Methods:
+      __init__, __repr__, check_sunk
+    """
+
     types = {"Carrier": 5, "Battleship": 4, "Destroyer": 3, "Submarine": 3,\
          "Patrol Boat": 2}
     
     def __init__(self, coordinates, type, battlefield):
+        """initialize an instance of a Ship.
+        
+        Args:
+          coordinates (list): the coordinates of the ship (where the ship will be placed on a Battlefield)
+          type (str): the name of the ship type, from the list of ship types.
+          battlefield (object): a Battlefiled class bject - the Battlefiled to which the ship will be added.
+        """
         self.coordinates = coordinates
         self.type = type
         self.battlefield = battlefield
@@ -482,9 +505,14 @@ class Ship:
             self.battlefield.grid[coordinate] = Battlefield.states[5]
 
     def __repr__(self):
+        """String representation of Ship containing it's type and coordinates"""
         return "Type " + str(self.type) + ". Coordinates: " + str(self.coordinates)
 
     def check_sunk(self):
+        """Check if the ship has sunk (if all of the ship's coordinates have been hit.
+        
+        If the ship has sunk, a stetement identifyuing the sunk ship is printed.
+        """
         hit_coordinates = 0
         battlefield = self.battlefield
         for coordinate in self.coordinates:
